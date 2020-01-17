28dfc715354960962afc722736da270c6eb13c89
milesziemer <45497130+milesziemer@users.noreply.github.com>
2020-01-17 00:21:14 -0500

    Delete php

--- head.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Website</title>
-    <link rel="stylesheet" href="stylesheet.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-</html>
